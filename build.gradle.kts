144f6ff5871e8404d79d77f0a63171b60a827ab0
PHSSGG <pedrohddgd@gmail.com>
2023-08-21 20:05:14 -0300

    added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 group = "phss.quizbot"
@@ -7,11 +8,21 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://jitpack.io/") }
 }
 
 dependencies {
+    implementation("com.github.discord-jda:JDA:v5.0.0-beta.13")
+    implementation("org.mongodb:mongodb-driver-sync:4.10.2")
+    implementation("com.googlecode.json-simple:json-simple:1.1.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "phss.quizbot.QuizBot"
+    }
 }
 
 tasks.getByName<Test>("test") {
